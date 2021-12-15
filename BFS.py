a70c80678ba67e69452953695432d972d370977a
shaayaansh <mmshokrii1996@gmail.com>
2021-12-15 14:21:49 -0500

    Distances Added to BFS, Shortest Path available

--- BFS.py
+++ BFS.py
@@ -14,16 +14,20 @@ def bfs(graph, node, visited):
     queue = []
     visited.append(node)
     queue.append(node)
-
+    distance = {}
+    distance[node] = "0"
+    counter = 0
     while queue:
         current = queue.pop(0)
         for i in range(len(graph[current])):
             if graph[current][i] not in visited:
                 visited.append(graph[current][i])
                 queue.append(graph[current][i])
+                distance[graph[current][i]] = int(distance[current]) + 1
                 print(graph[current][i], "Added to queue")
                 print("VISITED IS: ", visited)
         print("QUEUE IS : ", queue)
+        print("DISTANCES ARE : ", distance)
 
 
-bfs(graph, "A", visited)
+bfs(graph, "E", visited)
